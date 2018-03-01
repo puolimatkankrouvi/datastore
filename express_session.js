be741f8188bd4b47b0ad2097aef7b3ca8a4c4416
puolimatkankrouvi <cde_1414@yahoo.com>
2018-03-01 13:24:20 +0200

    sessions

--- express_session.js
+++ express_session.js
@@ -1,3 +1,31 @@
-const session = express-session
+const crypto = require('crypto');
+const session = require('express-session');
 
 
+
+function handleSession(app){
+
+  app.use(session({
+
+  	saveUninitialized: false,
+  	resave: false,
+    name:"id",
+    secret: crypto.randomBytes(64).toString('hex'),
+    cookie:{
+      path:'/',
+      maxAge: 1000*60*30, //30 mins
+      httpOnly: true,
+    },
+    //Using id instead of default connect.sid makes harder to determine the session mechanism
+    name: 'id'
+	}));
+
+	session.Session.prototype.login = function(user, next){
+		this.userInfo = user;
+		next();
+	}
+
+}
+
+
+module.exports = ({handleSession,})
--- node-abac/index.js
+++ node-abac/index.js
@@ -6,6 +6,7 @@
 
 const app = require('express')();
 const bodyParser = require('body-parser');
+const session = require('../express_session.js').handleSession(app);
 app.use(bodyParser.urlencoded({extended: true}));
 
 const authRoutes = require('./authRouter.js');
@@ -17,6 +18,18 @@ const PORT = 8000;
   Routes come here
 */
 
+app.get('/logout/', function(req,res,next){
+	if(req.session){
+		req.session.destroy( function(err,res){
+			if(!err){
+				return next;
+			}
+			else{
+				return res.json({message: "Logout successful"});
+			}
+		});
+	}
+});
 
 app.listen(PORT, () => {
   console.log('App running at localhost://%d', PORT);
