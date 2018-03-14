4087e3b4d31119a7ad184976b6bd28babc1191fa
puolimatkankrouvi <cde_1414@yahoo.com>
2018-03-14 21:26:18 +0200

    abac update routes

--- abac/abacRoutes.js
+++ abac/abacRoutes.js
@@ -4,6 +4,8 @@ const router = express.Router();
 const app_config = require('../app_config.js');
 const db = require('../db.js');
 const MongoStore = require('connect-mongo');
+const validator = require('form-validate');
+const appConfig = require('../app_config.js');
 
 
 /*Pug is used for views in templates-folder*/
@@ -35,6 +37,74 @@ function readData(req,res){
     }
   });
 };
+
+function createData(req,res){
+  req.Validator.filter('data_text', {
+    escapeHTML:true,
+    stripTags:true
+  })
+  .validate('data_text',{
+    length:{
+      min:appConfig.data_attr.min_length
+    }
+  });
+
+  req.Validator.getErrors( function(validator_errors){
+    //If errors back to create screen
+    if(validator_errors.length > 0){
+      var error = '';
+      for(err in validator_errors){
+        error += err + ' ';
+      }
+      redirect('/create', 200, {message: error});
+    }
+      //Insert the data to database
+      db.createData( req.Validator.getValue('data_text') );
+      redirect('/read', 200);
+    });
+}
+
+function updateGet(req,res){
+  db.readData(req.params.id, function(err,data){
+    if(err){
+      res.status(500);
+      res.json({error:'Error'});
+    }
+    res.render(
+      'update.pug',
+      {
+        'id': data._id,
+        'orginal_text': data.text
+      }
+    );
+  });
+};
+
+function updateData(req,res){
+  req.Validator.filter('data_text', {
+    escapeHTML:true,
+    stripTags:true
+  })
+  .validate('data_text',{
+    length:{
+      min:appConfig.data_attr.min_length
+    }
+  });
+
+  req.Validator.getErrors( function(validator_errors){
+    //If errors back to create screen
+    if(validator_errors.length > 0){
+      var error = '';
+      for(err in validator_errors){
+        error += err + ' ';
+      }
+      res.redirect('/create', {message: error});
+    }
+      //Insert the data to database
+      db.updateData( req.Validator.getValue('id') ,req.Validator.getValue('edited_text') );
+      res.redirect('/');
+    });
+}
 
 //Serverin tunniste
 const NAME = app_config.server_name;
@@ -73,9 +143,67 @@ const read = app.get('/', function(req, res, next){
 });
 
 
+const create_get = app.get('/create', function(req,res,next){
+  if(!req.user){
+    return res.redirect('/login');
+  }
+  abac.can(NAME, 'create', {
+    yes: function() {
+      res.render('create.pug');
+    },
+    no: function(err, info) {
+      unauthorized(req,res); 
+    }
+  })(req, res);
+});
 
+const create_post = app.post('/create', function(req,res,next){
+  if(!req.user){
+    return res.redirect('/login');
+  }
+  abac.can(NAME, 'create', {
+    yes: function() {
+      createData(req,res);
+    },
+    no: function(err, info) {
+      unauthorized(req,res); 
+    }
+  })(req, res);
+});
 
+const update_get = app.get('/edit/:id', function(req,res,next){
+  if(!req.user){
+    return res.redirect('/login');
+  }
+  abac.can(NAME, 'update', {
+    yes: function() {
+      updateGet(req,res);
+    },
+    no: function(err, info) {
+      unauthorized(req,res); 
+    }
+  })(req, res);
+});
 
+const update_post = app.post('/edit/:id', function(req,res,next){
+  if(!req.user){
+    return res.redirect('/login');
+  }
+  abac.can(NAME, 'update', {
+    yes: function() {
+      updateData(req,res);
+    },
+    no: function(err, info) {
+      unauthorized(req,res); 
+    }
+  })(req, res);
+});
 
-module.exports = ({read});
+module.exports = (
+  {
+    read,
+    create_post,
+    create_get
+  }
+);
 
